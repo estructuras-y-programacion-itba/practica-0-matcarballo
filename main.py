640d30ea7a48a28048188a4faa1acb91293d619a
matcarballo <matcarballo@itba.edu.ar>
2026-03-12 16:02:47 -0300

    commit

--- main.py
+++ main.py
@@ -38,4 +38,67 @@ def es_full(dados):
 def es_escalera(dados):
     valores = sorted(dados)
     return (valores == [1,2,3,4,5] or valores == [2,3,4,5,6])
-    
+
+planilla_j1 = {
+    "E": None,
+    "F": None,
+    "P": None,
+    "G": None,
+    "1": None,
+    "2": None,
+    "3": None,
+    "4": None,
+    "5": None,
+    "6": None
+}
+
+planilla_j2 = {
+    "E": None,
+    "F": None,
+    "P": None,
+    "G": None,
+    "1": None,
+    "2": None,
+    "3": None,
+    "4": None,
+    "5": None,
+    "6": None
+}
+
+def calcular_puntaje(dados, cat):
+    if cat == "G" or cat == "g":
+        if es_generala(dados):
+            return 50
+        else:
+            return 0
+    elif cat == "P" or cat == "p":
+        if es_poker(dados):
+            return 40
+        else:
+            return 0
+    elif cat == "F" or cat == "f":
+        if es_full(dados):
+            return 30
+        else:
+            return 0
+    elif cat == "E" or cat == "e":
+        if es_escalera(dados):
+            return 20
+        else:
+            return 0
+    else:
+        num = int(cat)
+        suma = 0
+        for dado in dados:
+            if dado == num:
+                suma += dado
+        return suma
+
+def turno(planilla):
+    dados = tirar_dados(5)
+    print(f"Dados: {dados[0]}, {dados[1]}, {dados[2]}, {dados[3]}, {dados[4]}")
+    cant_relanz = 0
+    relanz1 = input("Quiere relanzar? (S/N): ")
+    if relanz1 == "S" or relanz1 == "s":
+        cant_relanz += 1
+        
